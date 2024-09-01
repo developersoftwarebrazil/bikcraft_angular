1d9fa603ad78d810b947e28f8cf69ebf66185d0d
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-09-01 00:54:33 -0300

    criado o SEO da página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { FooterComponent } from '../../components/shared/footer/footer.component';
 import { HeadersComponent } from '../../components/shared/headers/headers.component';
 import { BicicletasListaComponent } from './bicicletas-lista/bicicletas-lista.component';
-import { MainComponent } from './main/main.component';
-import { TecnologiasComponent } from './tecnologias/tecnologias.component';
-import { ParceirosComponent } from './parceiros/parceiros.component';
 import { DepoimentosComponent } from './depoimentos/depoimentos.component';
+import { MainComponent } from './main/main.component';
+import { ParceirosComponent } from './parceiros/parceiros.component';
 import { SegurosComponent } from './seguros/seguros.component';
-import { FooterComponent } from '../../components/shared/footer/footer.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
 
 @Component({
   selector: 'home',
@@ -24,6 +25,18 @@ import { FooterComponent } from '../../components/shared/footer/footer.component
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(private titleService: Title, private metaService: Meta) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Bicicletas - Bikcraft');
+    this.metaService.addTags([
+      { name: 'description', content: 'Descubra nossas bicicletas artesanais de alta qualidade.' },
+      { name: 'keywords', content: 'bicicletas, artesanais, alta qualidade' },
+      { property: 'og:title', content: 'Bicicletas - Bikcraft' },
+      { property: 'og:description', content: 'Descubra nossas bicicletas artesanais de alta qualidade.' },
+      { property: 'og:type', content: 'website' },
+    ]);
+  }
 }
