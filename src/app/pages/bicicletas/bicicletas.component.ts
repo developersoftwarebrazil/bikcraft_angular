c4c71a0a380e500f896f5a32d50ba91f69c91e43
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-09-03 13:13:46 -0300

    criado o componente bicicletas

---
+++ src/app/pages/bicicletas/bicicletas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bicicletas',
+  standalone: true,
+  imports: [],
+  templateUrl: './bicicletas.component.html',
+  styleUrl: './bicicletas.component.scss'
+})
+export class BicicletasComponent {
+
+}
