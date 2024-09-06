44aa1fe6b3835fd90f06160e5d91b516607d4029
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-09-06 13:56:41 -0300

    criado o component seguro-components

---
+++ src/app/components/shared/seguro-components/seguro-components.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguro-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './seguro-components.component.html',
+  styleUrl: './seguro-components.component.scss'
+})
+export class SeguroComponentsComponent {
+
+}
