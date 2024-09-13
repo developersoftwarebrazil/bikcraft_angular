9f8123d86382876d164e5c5b4f3071fe653fd81b
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-09-12 22:09:49 -0300

    criado o component bicicleta-seguro

---
+++ src/app/components/shared/bicicleta-seguro/bicicleta-seguro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bicicleta-seguro',
+  standalone: true,
+  imports: [],
+  templateUrl: './bicicleta-seguro.component.html',
+  styleUrl: './bicicleta-seguro.component.scss'
+})
+export class BicicletaSeguroComponent {
+
+}
