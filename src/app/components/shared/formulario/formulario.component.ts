5749860be3ed994e08ae2afef97833c1ee8300ce
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-09-15 23:12:11 -0300

    finalizado o html do componente formulario e do componente contato

---
+++ src/app/components/shared/formulario/formulario.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'formulario',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ButtonComponent,
+  ],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.scss'
+})
+export class FormularioComponent {
+  public descricaoAriaLabelSection: string = 'Descrição da Seção';
+
+  @Input() sectionClass: string = 'formulario';
+
+  form: FormGroup;
+
+  constructor(private builder: FormBuilder) {
+    this.form = this.builder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      phone:['', [Validators.required, Validators.pattern('[0-9]+')]],
+    })
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log('form submited', this.form.value);
+    }
+    else {
+      console.log('Form is invalid');
+    }
+  }
+}
--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { HeadersComponent } from '../../components/shared/headers/headers.component';
+import { Component, Input } from '@angular/core';
 import { FooterComponent } from '../../components/shared/footer/footer.component';
+import { FormularioComponent } from '../../components/shared/formulario/formulario.component';
+import { HeadersComponent } from '../../components/shared/headers/headers.component';
 import { PageTitleComponent } from '../../components/shared/page-title/page-title.component';
 
 @Component({
@@ -11,11 +12,34 @@ import { PageTitleComponent } from '../../components/shared/page-title/page-titl
     CommonModule,
     HeadersComponent,
     PageTitleComponent,
+    FormularioComponent,
     FooterComponent,
   ],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.scss'
 })
 export class ContatoComponent {
+  public descricaoAriaLabelSection: string = 'Descrição da Seção';
 
+  @Input() sectionClass: string = 'contato-dados';
+
+  @Input() enderecoRua: string = "Rua: Analândia 474";
+  @Input() enderecoBairro: string = "São Joaquim";
+  @Input() enderecoCidade: string = "Santa Bárbara d'Oeste - SP";
+
+  @Input() numeroTelefone: number = 5519989327757;
+  @Input() telefone: string = "(19) 98932-5777";
+  @Input() enderecoEmail: string = "dnadevelopersoftware@gmail.com";
+  @Input() email: string = "@contato.bikcraft.com";
+  @Input() enderecoEmailAssistencia: string = "dnadevelopersoftware@gmail.com";
+  @Input() emailAssistencia: string = "@assistencia.bikcraft.com";
+
+  @Input() imgUrlInstagram: string = "assets/img/redes/instagram-p.svg";
+  @Input() instagramAltText: string = "Instagram";
+  @Input() imgUrlFace: string = "assets/img/redes/facebook-p.svg";
+  @Input() faceAltText: string = "Facebook";
+  @Input() imgUrlYoutube: string = "assets/img/redes/youtube-p.svg";
+  @Input() youtubeAltText: string = "Instagram";
+
+  @Input() formatoTelefone:string='+55 19 99999-9999'
 }
