9f1cae41213159577ff038753aa79c50d79aeabf
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-10-01 13:44:44 -0300

    nova estrurua inicial de html

--- src/app/pages/orcamentos/orcamentos.component.ts
+++ src/app/pages/orcamentos/orcamentos.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeadersComponent } from '../../components/shared/page-component/headers/headers.component';
+import { PageTitleComponent } from '../../components/shared/page-component/page-title/page-title.component';
+import { FooterComponent } from '../../components/shared/page-component/footer/footer.component';
+import { FormularioComponent } from '../../components/shared/page-component/formulario/formulario.component';
+
 
 @Component({
   selector: 'app-orcamentos',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    HeadersComponent,
+    PageTitleComponent,
+    FormularioComponent,
+    FooterComponent
+  ],
   templateUrl: './orcamentos.component.html',
   styleUrl: './orcamentos.component.scss'
 })
 export class OrcamentosComponent {
+
+  fieldFomsPersonal = [    // Seção de Dados Pessoais
+    { title: 'dados pessoais', divClass: 'font-1-xs cor-9 col-2' },
+
+    { name: 'nome', label: 'Nome', type: 'text', placeholder: 'Digite seu nome', required: true },
+    { name: 'sobrenome', label: 'Sobrenome', type: 'text', placeholder: 'Digite seu sobrenome', required: true },
+    { name: 'cpf', label: 'CPF', type: 'text', placeholder: '000.000.000-00', required: true, divClass: 'col-2' },
+    { name: 'email', label: 'Email', type: 'email', placeholder: 'Digite seu email', required: true, divClass: 'col-2' },
+
+    // Seção de Entrega
+    { title: 'entrega', divClass: 'font-1-xs cor-9 col-2' },
+
+    { name: 'cep', label: 'CEP', type: 'text', placeholder: 'Digite seu CEP', required: true },
+    { name: 'numero', label: 'Número', type: 'text', placeholder: 'Digite o número', required: true },
+    { name: 'logradouro', label: 'Logradouro', type: 'text', placeholder: 'Digite o logradouro', required: true },
+    { name: 'bairro', label: 'Bairro', type: 'text', placeholder: 'Digite o bairro', required: true },
+    { name: 'cidade', label: 'Cidade', type: 'text', placeholder: 'Digite a cidade', required: true },
+    { name: 'estado', label: 'Estado', type: 'text', placeholder: 'Digite o estado', required: true },
+
+  ];
 
 }
